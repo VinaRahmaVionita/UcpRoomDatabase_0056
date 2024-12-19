894f42ed870424ae5905661820434e6bfad3cafc
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-19 13:57:02 +0700

    mendeklarasikan operasi CRUD pada entitas Barang

--- app/src/main/java/com/example/ucp_2/data/repository/RepositoryBrg.kt
+++ app/src/main/java/com/example/ucp_2/data/repository/RepositoryBrg.kt
@@ -1,7 +1,12 @@
 package com.example.ucp_2.data.repository
 
 import com.example.ucp_2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryBrg {
     suspend fun insertBrg(barang: Barang)
+    suspend fun updateBrg(barang: Barang)
+    fun getBrg(id: String): Flow<Barang>
+    fun getAllBrg(): Flow<List<Barang>>
+    suspend fun deleteBrg(barang: Barang)
 }
