448251916a7816cb32fc578d70f0c1f98ae24281
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-19 13:49:45 +0700

    menambahkan parameter utama TopAppBar

--- app/src/main/java/com/example/ucp_2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp_2/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,15 @@
 package com.example.ucp_2.ui.customwidget
 
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier
+){
+
+}
