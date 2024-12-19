6801a89287d08f81cd48ced8e91fc0edef3ba20b
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-19 13:52:46 +0700

    mendefinisikan operasi create dan read pada data suplier

--- app/src/main/java/com/example/ucp_2/data/repository/LocalRepositorySpl.kt
+++ app/src/main/java/com/example/ucp_2/data/repository/LocalRepositorySpl.kt
@@ -1,4 +1,21 @@
 package com.example.ucp_2.data.repository
 
-class LocalRepositorySpl {
+import com.example.ucp_2.data.dao.SuplierDao
+import com.example.ucp_2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySpl (
+    private val suplierDao: SuplierDao
+) : RepositorySpl {
+    override suspend fun insertSpl(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+    override fun getAllSpl(): Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
+    }
+
+    override fun getSpl(id: String): Flow<Suplier> {
+        return suplierDao.getSuplier(id)
+    }
+
 }
