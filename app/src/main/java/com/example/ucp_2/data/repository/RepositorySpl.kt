ee090629298f424b5f61baee021a70550b4bf6da
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-19 13:57:24 +0700

    mendeklarasikan operasi CRUD pada entitas suplier

--- app/src/main/java/com/example/ucp_2/data/repository/RepositorySpl.kt
+++ app/src/main/java/com/example/ucp_2/data/repository/RepositorySpl.kt
@@ -1,4 +1,10 @@
 package com.example.ucp_2.data.repository
 
+import com.example.ucp_2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
 interface RepositorySpl {
+    suspend fun insertSpl(suplier: Suplier)
+    fun getAllSpl(): Flow<List<Suplier>>
+    fun getSpl(id: String): Flow<Suplier>
 }
