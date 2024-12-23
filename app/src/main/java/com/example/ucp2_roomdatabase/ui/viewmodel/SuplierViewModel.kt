89f3614dd4ce7fc02d5063328eef1cb0f81a77fa
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-24 00:08:41 +0700

    untuk mengelola logika penyimpanan data supplier

--- app/src/main/java/com/example/ucp2_roomdatabase/ui/viewmodel/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_roomdatabase/ui/viewmodel/SuplierViewModel.kt
@@ -9,7 +9,64 @@ import com.example.ucp2_roomdatabase.data.Repository.RepositorySpl
 import com.example.ucp2_roomdatabase.data.entity.Suplier
 import kotlinx.coroutines.launch
 
+//untuk mengelola logika penyimpanan data supplier 
+class SuplierViewModel (private val repositorySpl: RepositorySpl) : ViewModel() {
+    var uiState by mutableStateOf(SplUIState())
 
+    fun updateState(suplierEvent: SuplierEvent){
+        uiState = uiState.copy(
+            suplierEvent = suplierEvent
+        )
+    }
+
+    //validasi data input pengguna
+    private fun validateFields() : Boolean {
+        val event = uiState.suplierEvent
+        val errorStateSpl = FormErrorStateSpl(
+            idSpl = if (event.idSpl.isNotEmpty()) null else "Id Suplier tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama Suplier tidak boleh kosong",
+            kontak = if (event.kontak.isNotEmpty()) null else "Kontak Suplier tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat Suplier tidak boleh kosong"
+        )
+
+        uiState = uiState.copy(
+            isEntryValid = errorStateSpl
+        )
+        return errorStateSpl.isValid()
+    }
+
+    //Menyimpan data ke repository
+    fun saveSuplier() {
+        val currentEvent = uiState.suplierEvent
+
+        if (validateFields()) {
+                viewModelScope.launch {
+                    try {
+                        repositorySpl.insertSpl(currentEvent.toSuplierEntity())
+                        uiState = uiState.copy(
+                            snackBarMessage = "Data berhasil disimpan",
+                            suplierEvent = SuplierEvent(),
+                            isEntryValid = FormErrorStateSpl()
+                        )
+                    }catch (e: Exception) {
+                        uiState = uiState.copy(
+                            snackBarMessage = "Data gagal disimpan"
+                        )
+                    }
+                }
+            } else {
+                uiState = uiState.copy(
+                    snackBarMessage = "Input tidak valid. Periksa kembali data anda"
+                )
+
+        }
+    }
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(
+            snackBarMessage = null
+        )
+    }
+}
 
 //data class yang menyimpan berbagai informasi terkait UI
 data class SplUIState(
