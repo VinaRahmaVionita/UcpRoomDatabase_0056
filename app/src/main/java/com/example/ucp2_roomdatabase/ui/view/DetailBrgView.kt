b2c436a55ff5e80c1afa92ffda80c53f4d87dc80
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-23 21:50:10 +0700

    Dialog konfirmasi saat menghapus barang

---
+++ app/src/main/java/com/example/ucp2_roomdatabase/ui/view/DetailBrgView.kt
@@ -0,0 +1,67 @@
+package com.example.ucp2_roomdatabase.ui.view
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_roomdatabase.data.entity.Barang
+import com.example.ucp2_roomdatabase.ui.customwidget.TopAppBar
+import com.example.ucp2_roomdatabase.ui.viewmodel.DetailBrgViewModel
+import com.example.ucp2_roomdatabase.ui.viewmodel.DetailUiStateBrg
+import com.example.ucp2_roomdatabase.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2_roomdatabase.ui.viewmodel.toBarangEntity
+
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Delete Barang") },
+        text = { Text(" Apakah anda yakin ingin menghapus barang ini? ") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(
+                onClick = onDeleteCancel
+            ) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onDeleteConfirm
+            ) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
