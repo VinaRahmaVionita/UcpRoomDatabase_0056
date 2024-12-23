ed5e2fe85dd206b4fdb1ece19f71acf278d2ce10
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-23 22:27:55 +0700

    Menampilkan detail suplier di dalam card

---
+++ app/src/main/java/com/example/ucp2_roomdatabase/ui/view/HomeSuplierView.kt
@@ -0,0 +1,120 @@
+package com.example.ucp2_roomdatabase.ui.view
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_roomdatabase.data.entity.Suplier
+import com.example.ucp2_roomdatabase.ui.customwidget.TopAppBar
+import com.example.ucp2_roomdatabase.ui.viewmodel.HomeSuplierViewModel
+import com.example.ucp2_roomdatabase.ui.viewmodel.HomeUiStateSpl
+import com.example.ucp2_roomdatabase.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun CardSuplier(
+    suplier: Suplier,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    //mwndefinisakan warna kartu
+    val lightGray = Color.Gray
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = cardColors(containerColor = Color.Gray)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Default.DateRange, contentDescription = " ")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = suplier.idSpl,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Default.Person, contentDescription = " ")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = suplier.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Default.Phone, contentDescription = " ")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = suplier.kontak,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Default.LocationOn, contentDescription = " ")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = suplier.alamat,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
+}
